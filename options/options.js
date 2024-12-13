a61eab779cc811d55f2c9e45a7d4170b92956e64
Crowdbotics <team@crowdbotics.com>
2024-12-13 05:27:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-hall-50333",
+  url: "https://lucky-hall-50333.botics.co",
+  api: "https://lucky-hall-50333.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
